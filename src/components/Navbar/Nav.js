46427bb4cc10895b9abacb8f3ac6c8d897c31f13
upanshu21 <upanshu21@gmail.com>
2021-02-05 01:25:33 +0530

    added responsive navbar

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {IoIosArrowDropleft} from "react-icons/io"
 import './Nav.css'
 
 
+
 const Nav = () => {
+
+    const [open, setOpen] = useState(false)
     return (
         <div className="navbar">
             <div className="container">
@@ -12,7 +16,7 @@ const Nav = () => {
                         <img src="/logo.jpg" alt="" />
                     </div>
                 </ul>
-                <ul className="navbar__right">
+                <ul className="navbar__right" style={{transform: open ? "translateX(100%)" : "translateX(0%)"}} >
                     <li><a href="https://github.com/upanshu21/reactjs-portfolio">Home</a></li>
                     <li><a href="https://github.com/upanshu21/reactjs-portfolio">About</a></li>
                     <li><a href="https://github.com/upanshu21/reactjs-portfolio">Skills</a></li>
@@ -20,6 +24,7 @@ const Nav = () => {
                     <li><a href="https://github.com/upanshu21/reactjs-portfolio">Blog</a></li>
                     <li><a href="https://github.com/upanshu21/reactjs-portfolio">Contact</a></li>
                 </ul>
+                <IoIosArrowDropleft onClick={() => setOpen(!open) } className="slider" />
                 </div>
             </div>
         </div>
